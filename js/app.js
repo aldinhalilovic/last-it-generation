28c27a2812e3a6f0d5efe0ff42b925ef790d36f7
aldinhalilovic <aldinhalilovic06@gmail.com>
2024-03-16 15:57:43 +0100

    js started

---
+++ js/app.js
@@ -0,0 +1,116 @@
+// console.log("app.js loaded!");
+// console.log(17); number
+// console.log("17");
+// console.log(10); // number
+// console.log("10 + 10"); // string
+// console.log(true); // boolean
+// // console.log(false); // boolean
+// console.log(undefined);
+// console.log(null);
+
+// console.log(1 + 1);
+
+// console.log("1 + 1");
+
+// console.log("1" + 1);
+
+// console.log(1 + "1");
+// console.log("aldin");
+
+// sabiranje stringova
+
+// console.log("aldin" + "halilovic");
+// console.log("aldin" + " halilovic");
+
+// + ispred stringa convertuje string u number
+
+// console.log(1 + +"1");
+// console.log(1 + +"aldin");
+
+// typeof nesto   odredjuje tip podatka od /nesto/
+// console.log(typeof "aldin");
+// console.log(typeof true);
+// console.log(typeof typeof true);
+// console.log(typeof 0);
+
+// var;
+// let;
+// const;
+
+var a = "aldin";
+
+var b = "hamza";
+
+var prvaVarijabla;
+
+// console.log(prvaVarijabla);
+
+prvaVarijabla = "ali";
+
+// console.log(a);
+// console.log(b);
+
+// console.log(prvaVarijabla);
+
+// var 1 = 'aldin'
+
+var aldin_halilovic = 1; // snake case
+
+var aldinHalilovic = 2; // cammel case
+
+var URL = "https://nesto.com/users";
+
+var prva = 100;
+// console.log(prva);
+
+prva = 300;
+// console.log(prva);
+
+// let aldin;
+
+// console.log(aldin);
+// let aldin = "aldin ima 15 godina :)";
+// console.log(aldin);
+
+// aldin = "aldin nema vise 15 godina :))";
+// console.log(aldin);
+
+// const poslednja = "nemaa";
+
+// console.log(poslednja);
+
+// poslednja = "mozel ?";
+// console.log(poslednja);
+
+// console.log(mozda);
+// var mozda = "hocel ?";
+
+// console.log(nece);
+// let nece = "error";
+
+// == and ===
+// console.log(5 === 3);
+// console.log(5 === 5);
+
+// console.log(5 == "5");
+// console.log(5 == "3");
+
+// console.log(5 !== 3);
+// console.log(5 != "5");
+// console.log(5 !== 5);
+
+const c = "aldin";
+const d = "aldin";
+
+// console.log(c === d);
+
+//  and operator
+
+// console.log(false && 5);
+// console.log(5 && 3 && 10 && 5);
+
+// or operator
+
+console.log(false || !false);
+
+console.log(5 < 3);
